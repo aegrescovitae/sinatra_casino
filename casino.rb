c8b6d4a19e8476ee64eaa8b51985e2d5b5672ff8
Garrett Stot <garrettstott@gmail.com>
2016-02-29 10:59:02 -0700

    messing around

--- casino.rb
+++ casino.rb
@@ -1,8 +1,26 @@
 require 'sinatra'
 require 'pry'
+require_relative 'hi_lo'
 
 
 get '/' do
   erb :index
 end
 
+get '/hilo' do
+  @hilo = HiLo.new
+  erb :hi_lo
+end
+
+post '/hilo' do
+  params['@bet']
+  "Your bet is #{@bet}"
+  erb :hi_lo_number1
+
+end
+
+post '/hi_lo_number1' do
+  params ['@bet', '@guess']
+  erb :hi_lo_number2
+
+end
---
+++ hi_lo.rb
@@ -0,0 +1,9 @@
+class HiLo
+  def initialize
+    enter_bet_hilo
+  end
+
+  def enter_bet_hilo
+    @message = "Please enter your bet:"
+  end
+end
